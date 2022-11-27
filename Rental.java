4bbe117653c3c0a2887cbf279902745c5d5e3839
Raissa <84279343+raissamiranda@users.noreply.github.com>
2022-11-27 21:54:24 +0000

    Commit 4

--- Rental.java
+++ Rental.java
@@ -8,6 +8,27 @@ public class Rental {
       _daysRented = daysRented; 
    }
 
+ public double getCharge() {
+    // Adicionar o trecho de código extraído.
+    double thisAmount = 0;
+    switch (this.getMovie().getPriceCode()) {
+      case Movie.REGULAR:
+        thisAmount += 2;
+        if (this.getDaysRented() > 2)
+          thisAmount += (this.getDaysRented() - 2) * 1.5;
+        break;
+      case Movie.NEW_RELEASE:
+        thisAmount += this.getDaysRented() * 3;
+        break;
+      case Movie.CHILDRENS:
+        thisAmount += 1.5;
+        if (this.getDaysRented() > 3)
+          thisAmount += (this.getDaysRented() - 3) * 1.5;
+        break;
+    }
+    return thisAmount;
+  }
+
    public int getDaysRented() {
       return _daysRented;
    }
