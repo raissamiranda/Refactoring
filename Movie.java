f244ce3f90f9687a776f4a07b84028d5cb5635a2
Raissa <84279343+raissamiranda@users.noreply.github.com>
2022-11-27 22:36:30 +0000

    Commit 10

--- Movie.java
+++ Movie.java
@@ -44,4 +44,15 @@ public class Movie {
       }
       return thisAmount;
     }
+
+
+    public int getFrequentRenterPoints(int daysRented){
+      // add bonus for a two day new release rental
+      if ((getPriceCode() == Movie.NEW_RELEASE) &&
+              daysRented > 1){
+         return 2;
+      }
+      return 1;
+    }
+  
 }
